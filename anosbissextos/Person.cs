82590849f66cd59af46fc8c9926a15f8dbad9a20
luisbrandino <luisbrandino.contato@gmail.com>
2024-05-05 22:32:06 -0300

    Add project files.

---
+++ anosbissextos/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace anosbissextos
+{
+    internal class Person
+    {
+        string name;
+        string profession;
+        int age;
+
+        public Person(string name, string profession, int age)
+        {
+            this.name = name;
+            this.profession = profession;
+            this.age = age;
+        }
+
+        public string GetName() { return name; }
+        
+        public string GetProfession() { return profession; }
+
+        public int GetAge() { return age; }
+
+        public int GetAgeInLeapYears()
+        {
+            int birthYear = DateTime.Now.Year - this.age;
+            int leapYears = 0;
+
+            for (int i = birthYear; i < DateTime.Now.Year; i++)
+            {
+                bool isLeapYear = (i % 4 == 0 && i % 100 != 0) || (i % 400 == 0);
+
+                if (isLeapYear)
+                    leapYears++;
+            }
+
+            return leapYears;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {this.name}\nProfissão: {this.profession}\nIdade: {this.age}";
+        }
+    }
+}
---
+++ anosbissextos/Program.cs
@@ -0,0 +1,61 @@
+﻿using anosbissextos;
+int inputInteger(string message, int? min = null, int? max = null)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+
+    while (true)
+    {
+        bool correctValue = (min != null ? value >= min : true) && (max != null ? value <= max : true);
+
+        if (correctValue)
+            break;
+
+        string invalid = min != null ? $"Valor precisa ser maior ou igual a {min}" : "";
+        invalid += invalid == "" ? "Valor precisa ser " : max != null ? " e " : "";
+        invalid += max != null ? $"menor ou igual a {max}" : "";
+        invalid += ": ";
+
+        Console.Write(invalid);
+        value = int.Parse(Console.ReadLine());
+    }
+
+    return value;
+}
+
+void waitForAnyKey()
+{
+    Console.WriteLine("\nPressione qualquer tecla para continuar.");
+    Console.ReadKey();
+}
+
+Person createPerson()
+{
+    Console.Write("Digite o nome: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Digite a profissão: ");
+    string profession = Console.ReadLine();
+
+    int age = inputInteger("Digite a idade: ", min: 0);
+
+    return new Person(name, profession, age);
+}
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("[ 1 ] Criar nova pessoa\n[ 0 ] Sair");
+    int option = inputInteger("Sua opção: ", min: 0, max: 1);
+
+    if (option == 0)
+        break;
+
+    Console.Clear();
+    Person person = createPerson();
+
+    Console.WriteLine(person);
+    Console.WriteLine($"Idade em anos bissextos: {person.GetAgeInLeapYears()}");
+
+    waitForAnyKey();
+}
